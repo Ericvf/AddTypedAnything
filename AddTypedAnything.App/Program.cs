e12779c5f957318864dfd6d35303fec578d142bb
Ericvf <ericvf@fegelein.com>
2022-01-04 01:59:16 +0100

    Initial version

---
+++ AddTypedAnything.App/Program.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AddTypedAnything
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceProvider = BuildServiceProvider();
+            var application = serviceProvider.GetRequiredService<App>();
+            await application.RunAsync(args);
+        }
+
+        private static ServiceProvider BuildServiceProvider()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(AppContext.BaseDirectory))
+            .AddJsonFile("appsettings.json", optional: false);
+
+            var configurationRoot = builder.Build();
+
+            var configSetting2 = new ConfigSetting();
+            configurationRoot.Bind("ConfigSetting2", configSetting2);
+
+            return new ServiceCollection()
+                .Configure<ConfigSetting>(configurationRoot.GetSection(key: nameof(ConfigSetting)))
+                .AddSingleton<App>()
+                .AddSingleton<TestServiceA>()
+                .AddSingleton<TestServiceB>()
+                .AddSingleton<TestServiceC>()
+                .AddSingleton<ITest, TestServiceC>()
+                .AddSingleton<SubstituteWithServiceA>(pb => pb.Type<TestServiceA>())
+                .AddSingleton<SubstituteWithServiceB>(pb => pb.Type<TestServiceB>())
+                .AddSingleton<IServiceBase, SubstituteWithServiceAAndOptions>(pb => pb
+                    .Value("Injected string")
+                    .Value(Options.Create(configSetting2))
+                    .Type<TestServiceA>())
+                .BuildServiceProvider();
+        }
+    }
+
+    public class ConfigSetting
+    {
+        public string Name { get; set; }
+
+    }
+
+    public class TestServiceA : ITest
+    {
+        public TestServiceA(IOptions<ConfigSetting> bla)
+        {
+            Console.WriteLine("TestServiceA: " + GetHashCode());
+        }
+    }
+
+    public class TestServiceB : ITest
+    {
+        public TestServiceB()
+        {
+            Console.WriteLine("TestServiceB: " + GetHashCode());
+        }
+    }
+
+    public class TestServiceC : ITest
+    {
+        public TestServiceC()
+        {
+            Console.WriteLine("TestServiceC: " + GetHashCode());
+        }
+    }
+
+    public interface ITest
+    {
+    }
+
+    public class SubstituteWithServiceB
+    {
+        public SubstituteWithServiceB(ITest test)
+        {
+            Console.WriteLine("SubstituteWithServiceB:" + test);
+        }
+    }
+
+    public class SubstituteWithServiceA
+    {
+        public SubstituteWithServiceA(ITest test)
+        {
+            Console.WriteLine("SubstituteWithServiceA:" + test);
+        }
+    }
+
+    public class SubstituteWithServiceC : IServiceBase
+    {
+        public SubstituteWithServiceC(ITest test)
+        {
+            Console.WriteLine("SubstituteWithServiceC:" + test);
+        }
+    }
+    public class SubstituteWithServiceAAndOptions : IServiceBase
+    {
+        public SubstituteWithServiceAAndOptions(ITest test, string input, IOptions<ConfigSetting> configSettings2)
+        {
+            Console.WriteLine("SubstituteWithServiceAAndOptions:" + test);
+        }
+    }
+    public interface IServiceBase
+    {
+    }
+
+    public class App
+    {
+        public App(ITest test, SubstituteWithServiceA substituteWithServiceA, SubstituteWithServiceB substituteWithServiceB, IServiceBase serviceBase, SubstituteWithServiceB substituteWithServiceB2)
+        {
+        }
+
+        public async Task RunAsync(string[] args)
+        {
+            await Task.Delay(0);
+        }
+    }
+}
---
+++ AddTypedAnything/ServiceCollectionExtensions.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddTypedAnything
+{
+    public class ParameterBuilder
+    {
+        private readonly IList<IParameter> parameters = new List<IParameter>();
+
+        private interface IParameter
+        {
+        }
+
+        private class ValueParameter : IParameter
+        {
+            public object Value { get; set; }
+        }
+
+        private class TypeParameter : IParameter
+        {
+            public Type ImplementationType { get; set; }
+        }
+
+        public ParameterBuilder Value(object obj)
+        {
+            parameters.Add(new ValueParameter()
+            {
+                Value = obj
+            });
+            return this;
+        }
+
+        public ParameterBuilder Type<TImplementation>()
+        {
+            parameters.Add(new TypeParameter()
+            {
+                ImplementationType = typeof(TImplementation)
+            });
+            return this;
+        }
+
+        public IEnumerable<object> Build(IServiceProvider serviceProvider)
+        {
+            foreach (var item in parameters)
+            {
+                switch (item)
+                {
+                    case ValueParameter valueParameter:
+                        yield return valueParameter.Value;
+                        break;
+
+                    case TypeParameter typeParameter:
+                        yield return serviceProvider.GetRequiredService(typeParameter.ImplementationType);
+                        break;
+                }
+            }
+        }
+    }
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSingleton<TTargetImplementation>(this IServiceCollection serviceCollection, Action<ParameterBuilder> builder)
+          where TTargetImplementation : class
+          => serviceCollection.AddSingleton(serviceProvider => Resolve<TTargetImplementation, TTargetImplementation>(serviceProvider, builder));
+
+        public static IServiceCollection AddScoped<TTargetImplementation>(this IServiceCollection serviceCollection, Action<ParameterBuilder> builder)
+          where TTargetImplementation : class
+          => serviceCollection.AddScoped(serviceProvider => Resolve<TTargetImplementation, TTargetImplementation>(serviceProvider, builder));
+
+        public static IServiceCollection AddTransient<TTargetImplementation>(this IServiceCollection serviceCollection, Action<ParameterBuilder> builder)
+          where TTargetImplementation : class
+          => serviceCollection.AddTransient(serviceProvider => Resolve<TTargetImplementation, TTargetImplementation>(serviceProvider, builder));
+
+        public static IServiceCollection AddSingleton<TTargetInterface, TTargetImplementation>(this IServiceCollection serviceCollection, Action<ParameterBuilder> builder)
+          where TTargetInterface : class
+          where TTargetImplementation : class, TTargetInterface
+          => serviceCollection.AddSingleton(serviceProvider => Resolve<TTargetInterface, TTargetImplementation>(serviceProvider, builder));
+
+        public static IServiceCollection AddScoped<TTargetInterface, TTargetImplementation>(this IServiceCollection serviceCollection, Action<ParameterBuilder> builder)
+          where TTargetInterface : class
+          where TTargetImplementation : class, TTargetInterface
+          => serviceCollection.AddScoped(serviceProvider => Resolve<TTargetInterface, TTargetImplementation>(serviceProvider, builder));
+
+        public static IServiceCollection AddTransient<TTargetInterface, TTargetImplementation>(this IServiceCollection serviceCollection, Action<ParameterBuilder> builder)
+          where TTargetInterface : class
+          where TTargetImplementation : class, TTargetInterface
+          => serviceCollection.AddTransient(serviceProvider => Resolve<TTargetInterface, TTargetImplementation>(serviceProvider, builder));
+
+        private static TTargetInterface Resolve<TTargetInterface, TTargetImplementation>(IServiceProvider serviceProvider, Action<ParameterBuilder> builder)
+            where TTargetInterface : class
+            where TTargetImplementation : class, TTargetInterface
+        {
+            var parameterBuilder = new ParameterBuilder();
+            builder(parameterBuilder);
+            var parameters = parameterBuilder.Build(serviceProvider);
+            return ActivatorUtilities.CreateInstance<TTargetImplementation>(serviceProvider, parameters.ToArray());
+        }
+    }
+}
