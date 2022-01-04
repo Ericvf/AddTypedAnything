3c9209653432b710e31033bb7abcc75b9fc5aac2
Ericvf <ericvf@fegelein.com>
2022-01-04 21:01:05 +0100

    Rename

--- DependencyInjection.Extensions.Parameterization.App/Program.cs
+++ DependencyInjection.Extensions.Parameterization.App/Program.cs
@@ -5,7 +5,7 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 
-namespace AddTypedAnything
+namespace DependencyInjection.Extensions.Parameterization.App
 {
     class Program
     {
--- DependencyInjection.Extensions.Parameterization/ServiceCollectionExtensions.cs
+++ DependencyInjection.Extensions.Parameterization/ServiceCollectionExtensions.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace AddTypedAnything
+namespace DependencyInjection.Extensions.Parameterization
 {
     public class ParameterBuilder
     {
