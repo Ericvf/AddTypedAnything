081b5ce2e9d5e1e8239bd017a037b04073f279f8
Ericvf <ericvf@fegelein.com>
2022-01-04 22:24:40 +0100

    Refactoring

--- DependencyInjection.Extensions.Parameterization/ServiceCollectionExtensions.cs
+++ DependencyInjection.Extensions.Parameterization/ServiceCollectionExtensions.cs
@@ -1,91 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace DependencyInjection.Extensions.Parameterization
 {
-    public class ParameterBuilder
-    {
-        private readonly IList<IParameter> parameters = new List<IParameter>();
-
-        private interface IParameter
-        {
-        }
-
-        private class ValueParameter : IParameter
-        {
-            public object Value { get; set; }
-        }
-
-        private class TypeParameter : IParameter
-        {
-            public Type ImplementationType { get; set; }
-        }
-
-        private class OptionsParameter : IParameter
-        {
-            public Func<IConfigurationRoot, object> OptionFactory { get; set; }
-        }
-
-        public ParameterBuilder Value(object obj)
-        {
-            parameters.Add(new ValueParameter()
-            {
-                Value = obj
-            });
-            return this;
-        }
-
-        public ParameterBuilder Type<TImplementation>()
-        {
-            parameters.Add(new TypeParameter()
-            {
-                ImplementationType = typeof(TImplementation)
-            });
-            return this;
-        }
-
-        public ParameterBuilder Options<TImplementation>(string key)
-            where TImplementation : class, new()
-        {
-            parameters.Add(new OptionsParameter()
-            {
-                OptionFactory = (configurationRoot) => {
-                    var instance = new TImplementation();
-                    configurationRoot.Bind(key, instance);
-                    return Microsoft.Extensions.Options.Options.Create(instance);
-                }
-
-            });
-            return this;
-        }
-
-        public IEnumerable<object> Build(IServiceProvider serviceProvider)
-        {
-            var configurationRoot = new Lazy<IConfigurationRoot>(() => serviceProvider.GetRequiredService<IConfigurationRoot>());
-
-            foreach (var item in parameters)
-            {
-                switch (item)
-                {
-                    case ValueParameter valueParameter:
-                        yield return valueParameter.Value;
-                        break;
-
-                    case TypeParameter typeParameter:
-                        yield return serviceProvider.GetRequiredService(typeParameter.ImplementationType);
-                        break;
-
-                    case OptionsParameter optionsParameter:
-                        yield return optionsParameter.OptionFactory(configurationRoot.Value);
-                        break;
-                }
-            }
-        }
-    }
-
     public static class ServiceCollectionExtensions
     {
         public static IServiceCollection AddSingleton<TTargetImplementation>(this IServiceCollection serviceCollection, Action<ParameterBuilder> builder)
